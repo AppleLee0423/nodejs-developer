ccb1c615a68b09c17516c40589ac70130cdf20fa
AppleLee0423 <ld210@naver.com>
2023-08-13 23:42:24 +0900

    board write

--- express-board/app.js
+++ express-board/app.js
@@ -8,7 +8,11 @@ app.set("views", __dirname + "/views"); //뷰 디렉터리를 views로 설정
 
 //라우터 설정
 app.get("/", (req, res) => {
-    res.render("home", {title: "안녕하세요", message: "만나서 반갑습니다!"});
+    res.render("home", {title: "테스트 게시판", message: "만나서 반갑습니다!"});
+});
+
+app.get("/write", (req, res) => {
+    res.render("write", {title: "테스트 게시판"});
 });
 
 app.listen(3000);
