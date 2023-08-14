740f11f00c7943ec75d41649af826f9f9815c513
AppleLee0423 <ld210@naver.com>
2023-08-15 00:11:16 +0900

    ArticleList paginator

--- express-board/app.js
+++ express-board/app.js
@@ -18,8 +18,16 @@ app.set("view engine", "handlebars"); //웹페이지 로드 시 사용할 템플
 app.set("views", __dirname + "/views"); //뷰 디렉터리를 views로 설정
 
 //라우터 설정
-app.get("/", (req, res) => {
-    res.render("home", {title: "테스트 게시판", message: "만나서 반갑습니다!"});
+app.get("/", async (req, res) => {
+    const page = parseInt(req.query.page) || 1; //현재 페이지 데이터
+    const search = req.query.search || ""; //검색어 데이터
+    try {
+        const [posts, paginator] = await postService.list(collection, page, search);
+        res.render("home", {title: "EXPRESS 게시판", search, paginator, posts});
+    } catch (error) {
+        console.error(error);
+        res.render("home", {title: "테스트 게시판"});
+    }
 });
 
 app.get("/write", (req, res) => {
--- express-board/services/post-service.js
+++ express-board/services/post-service.js
@@ -1,3 +1,22 @@
+const paginator = require("../utils/paginator");
+
+//글 목록
+async function list(collection, page, search) {
+    const perPage = 10;
+    const query = {title: new RegExp(search, "i")}; //title이 search와 부분일치 하는지 확인
+    /**
+     * limit는 10개씩 가져온다는 의미, skip은 설정된 갯수만큼 건너뜀, 생성일 역순 정렬
+     */
+    const cursor = collection.find(query, {limit: perPage, skip: (page-1)*perPage}).sort({createdAt: -1,});
+
+    const totalCount = await collection.count(query);
+    const posts = await cursor.toArray(); //데이터를 리스트로 변경
+
+    const paginatorObj = paginator({totalCount, page, perPage: perPage});
+    return [posts, paginatorObj];
+}
+
+//새 글 작성
 async function writePost(collection, post) {
     /**
      * 조회수, 생성일자(ISO포맷)
@@ -8,5 +27,6 @@ async function writePost(collection, post) {
 }
 
 module.exports = {
+    list,
     writePost,
 };
